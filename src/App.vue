c2a08be918457fadde54706b218bdd63688f3a35
shiropt <63380875+shiropt@users.noreply.github.com>
2021-09-05 11:24:40 +0900

    Data

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <task-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TaskList from "./components/TaskList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    TaskList,
+  },
 };
 </script>
 
