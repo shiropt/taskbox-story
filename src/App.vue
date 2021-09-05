90e8c3faa01f7c2a5f88dc8f4e29eb495d24491b
shiropt <63380875+shiropt@users.noreply.github.com>
2021-09-05 13:42:29 +0900

    Screens

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <task-list />
+    <InboxScreen />
   </div>
 </template>
 
 <script>
-import TaskList from "./components/TaskList.vue";
-
+import InboxScreen from "./components/InboxScreen.vue";
 export default {
   name: "App",
   components: {
-    TaskList,
+    InboxScreen,
   },
 };
 </script>
